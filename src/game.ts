c6f2b167c55184688486b84ba49c53a7a2470731
Ilmi Kerimov <kerimov200002@gmail.com>
2022-02-07 19:37:41 +0300

    Added processing of images

--- src/game.ts
+++ src/game.ts
@@ -1,78 +1,109 @@
 import * as utils from '@dcl/ecs-scene-utils'
 
 
-function create_display(link: string){
-    myVideoClip = new VideoClip(link)
+function create_display(stream: any){
+    if (stream.is_image){
+        myImageTexture = new Texture(stream.url)
 
-    myVideoTexture = new VideoTexture(myVideoClip)
-    myVideoTexture.loop = true
+        myMaterial.albedoTexture = myImageTexture
 
-    myMaterial.albedoTexture = myVideoTexture 
+        video_rotation = new Quaternion(360, 1, 360, 1)
+    }
+    else {
+        myVideoClip = new VideoClip(stream.url)
+
+        myVideoTexture = new VideoTexture(myVideoClip)
+        myVideoTexture.loop = true
+        myMaterial.albedoTexture = myVideoTexture
+        
+        myVideoTexture.play()
+        
+        video_rotation = new Quaternion(0, -1, 0, 1)
+    }    
+
+    screen = new Entity()
+    screen.addComponent(new PlaneShape())
+    screen.addComponent(
+        new Transform({
+            position: video_position,
+            scale: video_scale,
+            rotation: video_rotation,
+        })
+    )
+    screen.addComponent(myMaterial)
+    engine.addEntity(screen)
 }
 
 
-async function get_data(minute: number) {
-    // let json = await utils.sendRequest(
-    //     'https://worldtimeapi.org/api/timezone/etc/gmt+3'
-    // )
-    // let toDate = new Date(json.datetime)
-    // log(toDate)
-    // log(toDate.getSeconds())
-    
-    var link: string
-    if (minute % 2 == 0){
-        link = "https://vod-progressive.akamaized.net/exp=1643990950~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3195%2F19%2F490977203%2F2205504628.mp4~hmac=afae8c846327abae24598fc136025ed2d8c7dda2e1cc93973fd6d437c80145de/vimeo-prod-skyfire-std-us/01/3195/19/490977203/2205504628.mp4?filename=Moon+-+59026.mp4"
-        log('чётное')
+function DateWithoutTimeZone(string_date: string) {
+    let date = new Date(string_date)
+    if (date == null) return date
+    var timestamp = date.getTime() - date.getTimezoneOffset() * 60000
+    var correctDate = new Date(timestamp)
+    return correctDate
+}
+
+
+async function get_data() {
+    let json = await utils.sendRequest(
+        'https://metaads.team/tornado/adspot/id/1/stream',
+        'GET',
+        {
+            'Host': 'metaads.team',
+            'Connection': 'keep-alive',
+            'Cache-Control': 'max-age=0',
+            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Yandex";v="22"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.116 YaBrowser/22.1.1.1544 Yowser/2.5 Safari/537.3',
+            'Accept': '*/*',
+            'Sec-Fetch-Site': 'none',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-User': '?1',
+            'Sec-Fetch-Dest': 'document',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en;q=1'
+        }
+    )
+    let message = json.msg
+    if (message && message == 'There are no stream now'){
+        log('Nothing to show at the moment')
     }
     else {
-        link = "https://vod-progressive.akamaized.net/exp=1643991025~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3353%2F25%2F641767435%2F2942139256.mp4~hmac=f4f47477cba6174909c8f02be6a40fa8811437669460e1e593d300a8e33004bd/vimeo-prod-skyfire-std-us/01/3353/25/641767435/2942139256.mp4?filename=Ocean+-+93745.mp4"
-        log('нечётное')
+        update_date = DateWithoutTimeZone(json.to_time)
+        create_display(json)
     }
-    
-    create_display(link)
-    myVideoTexture.play()
 }
 
 
 var myVideoClip: VideoClip
 var myVideoTexture: VideoTexture
+var myImageTexture: Texture
 var myMaterial: Material
 var screen: Entity
 
-let video_scale = new Vector3(8, 4.5, 1)
-let video_position = new Vector3(15, 1 + video_scale.y / 2, 4 + video_scale.x / 2)
-let video_rotation = new Quaternion(0, 1, 0, 1)
+var video_scale = new Vector3(8, 4.5, 1)
+var video_position = new Vector3(15, 1 + video_scale.y / 2, 4 + video_scale.x / 2)
+var video_rotation = null
 
+var update_date: any = null
 
 myMaterial = new Material()
 myMaterial.roughness = 1
 myMaterial.specularIntensity = 0
 myMaterial.metallic = 0
 
-screen = new Entity()
-screen.addComponent(new PlaneShape())
-screen.addComponent(
-    new Transform({
-        position: video_position,
-        scale: video_scale,
-        rotation: video_rotation,
-    })
-)
-screen.addComponent(myMaterial) 
-
 
 let checker = new Entity()
 engine.addEntity(checker)
 checker.addComponent(
-  new utils.Interval(1000, () => {
+  new utils.Interval(5000, () => {
     var now = new Date();
-    if (now.getSeconds() == 0) {
+    if (update_date == null || update_date < now){
         executeTask(async () => {
-           await get_data(now.getMinutes())
+            await get_data()
         })
-        log('new minute')
     }
-    log(now)
   })
 )
-engine.addEntity(screen)
